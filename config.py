b34f887ab6a7455f2208d6b2faf4ee4f54e1f321
rohit87099 <155307408+rohit87099@users.noreply.github.com>
2024-03-13 19:26:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6709762784:AAEXGyO9luxHMGl2IA8jC60kLtjM_1bFeP8")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6744570758:AAEKlYipGDT9BVcLNq7t8QsxXSEd2InnU-c")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "29459252"))
